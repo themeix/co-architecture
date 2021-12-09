fe15f8098b38f02c20545bd9802dca57c377e1d3
themeix <themeix@gmail.com>
2021-12-09 11:38:27 +0600

    design

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+(function ($) {
+  "use strict";
+  /*
+------------------------  
+Preloader
+--------------------------
+*/
+  $(window).on('load', function () {
+    var preLoder = $(".preloader");
+    preLoder.fadeOut(10);
+  });
+  /*
+  ------------------------  
+  Sticky Header 
+  --------------------------
+  */
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 0) {
+      $(".header-bottombar").addClass("header-sticky");
+    } else {
+      $(".header-bottombar").removeClass("header-sticky");
+    }
+  });
+
+   /*  Active Class*/
+   $('.header-cart,.sidebar-close').on('click', function() {
+    $("#sidebar-navbar-area").toggleClass('active');
+    $("body").toggleClass('sidebar-before-bg');
+});
+  /*
+  ------------------------  
+  Mobile Menu 
+  --------------------------
+  */
+  $(".mobile-toggle").on("click", function () {
+    $(this).toggleClass("open");
+    $(".toggle-menu-class").slideToggle();
+  });
+  $(".toggle").on("click", function () {
+    if ($(this).text().includes("-")) {
+      $(this).text("+")
+    } else {
+      $(this).text("-")
+    }
+    $(this).parent().siblings(".submenu").slideToggle();
+  });
+
+
+
+
+ 
+
+  /*
+  ------------------------  
+  AOS Animation
+  --------------------------
+  */
+  AOS.init({
+    offset: 50,
+    delay: 0,
+    duration: 400,
+    easing: 'ease-in-out',
+    debounceDelay: 20,
+    throttleDelay: 50,
+    once: true,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+  });
+ 
+
+}(jQuery));
---
+++ tailwind.config.js
@@ -0,0 +1,160 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./*.html'],
+  darkMode: false,
+   corePlugins: {
+    container: true 
+  },
+    plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '610px',
+          },
+          '@screen md': {
+            maxWidth: '750px',
+          },
+          '@screen lg': {
+            maxWidth: '1000px',
+          },
+          '@screen xl': {
+            maxWidth: '1200px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1316px', 
+          },
+        }
+      })
+    } 
+  ],
+  theme: {
+    extend: {
+      container: { 
+        center: true,
+        padding: '1rem',
+       }, 
+      colors: {
+        "indigo-500": "#7d7c7d",
+        "black": "#000000",
+        "black-200": "   #1d1d1f",
+        'yellow-100': '#ffcc00',
+        'yellow-200': '#f7dfce',
+        'yellow-300': '#f5f4f6',
+        
+     
+        
+        
+      },
+      backgroundImage: {
+        "curved-blue-gradient": "url(/imagenes/precios/curved-blue-gradient-bg.svg)",
+        "blue-cyan-gradient": "linear-gradient(90deg, #C84E89 0%, #F15F79 100%);",
+        "border-gradient": "linear-gradient(90deg, #C84E89 0%, #F15F79 100%);",
+      
+          },
+
+
+      placeholderColor: theme => theme('colors'),
+      placeholderColor: {
+        "coolGray-600":"#4B5563",
+      },
+      borderColor: theme => ({
+        ...theme('colors'),
+         DEFAULT: theme('colors.gray.300', 'currentColor'),
+        'coolGray-100': '#f5f4f6',
+        'yellow-300': '#FDE047',
+       
+       }),
+       borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+       '3': '3px',
+        '4': '4px',
+       '5': '5px',
+       '8': '8px',
+      },
+     
+      borderRadius: {
+        'none': '0',
+        DEFAULT: '0',
+        'sm': '0.25rem',
+        'md': '8px',
+        'lg': '1.25rem',
+        'xl': '0'
+      },
+      
+       fontFamily: { 
+        sans: ['"Roboto", sans-serif'],  
+        display: ['"Lato", sans-serif'],
+        body: ['"Roboto", sans-serif'],   
+      },
+      fontSize: {
+        'xs': '13px',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+         'base': '1rem',
+         'lg': '1.125rem',
+         'xl': '1.25rem',
+         '2xl': '28px',
+        '3xl': '2rem',
+        '4xl': '2.5rem',
+         '5xl': '3rem',
+         '6xl': '3.5rem',
+        '7xl': '4rem',
+        '18': '18px',
+       },
+      boxShadow: {
+        base:' 0px 1px 3px 0px #0000001A',
+        none: 'none',
+        
+      },
+      maxWidth: {
+        '62.2': '200px',
+      
+      },
+      width: {
+        '15': '3.75rem',
+        '168': '42rem',
+        '87.5': '350px',
+        '38.5': '154px',
+        
+        
+       
+      },
+      height: {
+        '15': '3.75rem',
+        '35': '35px',
+       
+       },
+       scale: {
+        '0': '0',
+       '25': '.25',
+      },
+      zIndex: {
+        '-9999': '-9999',
+        '0': 0,
+        '1': -1,
+        '2': 2,
+        'auto': 'auto',
+      },
+      inset: {
+        '45': "180px",
+        
+
+      },
+      opacity: {
+        '60': '60%',
+        
+       },
+       lineHeight: {
+        '0': '0',
+        '13': '13px',
+       
+ 
+       }
+    },
+  },
+  // Other stuff
+};
